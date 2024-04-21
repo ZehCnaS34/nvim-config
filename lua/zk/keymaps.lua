c0b292589049f77529742db31084895af7806cba
Alexander Sanchez <alex@violet.city>
2024-04-20 23:42:15 -0700

    experiement with keymap constructions

--- lua/zk/keymaps.lua
+++ lua/zk/keymaps.lua
@@ -1,6 +1,21 @@
 local scope = {}
 
+local function description_to_command(desc)
+    local words = vim.split(desc, " ")
+    local chars = {}
+
+    for i, word in ipairs(words) do
+        chars[i] = string.sub(word, 1, 1)
+    end
+
+    return "<leader>" .. vim.iter(chars):join("")
+end
+
 scope.keymaps = {
+    basic = function(desc, action)
+        local lhs = description_to_command(desc)
+        vim.keymap.set('n', lhs, action, { nowait = true, desc = desc })
+    end,
     insert = function(lhs, rhs, desc)
         vim.keymap.set('i', lhs, rhs, { nowait = true, desc = desc })
     end,
@@ -21,8 +36,8 @@ scope.setup = function(config)
 
     -- reload current lua file
     -- TODO: make this only work for lua files...
-    km.normal(
-        '<leader>cr', -- not the best choice
+    km.basic(
+        'config reload', -- not the best choice
         function()
             vim.cmd [[:source %]]
             vim.cmd [[:echom "[reloaded] ".expand("%")]]
@@ -31,10 +46,10 @@ scope.setup = function(config)
 
     -- project navigation
     km.normal('<leader><leader>', require('telescope.builtin').buffers)
-    km.normal('<leader>pf', require('telescope.builtin').find_files)
-    km.normal('<leader>pb', require('telescope.builtin').buffers, "[P]roject [B]uffers")
+    km.basic('project files', require('telescope.builtin').find_files)
+    km.basic('project buffers', require('telescope.builtin').buffers)
+    km.basic('project grep', require('telescope.builtin').live_grep)
     km.normal('<leader>pt', require('telescope.builtin').builtin)
-    km.normal('<leader>/', require('telescope.builtin').live_grep)
 
     km.normal('<leader>gs', ':Neogit<cr>')
 
@@ -46,13 +61,13 @@ scope.setup = function(config)
     -- conjure. this should be only applied in a particular mode
     km.normal('<leader>mee', ':ConjureEvalRootForm<cr>')
 
-    km.normal('<leader>ws', ':split<cr>', 'Split window')
-    km.normal('<leader>wv', ':vsplit<cr>', 'Split window vertically')
-    km.normal('<leader>wh', '<C-w>h', 'Focus on left window')
-    km.normal('<leader>wj', '<C-w>j', 'Focus on down window')
-    km.normal('<leader>wk', '<C-w>k', 'Focus on up window')
-    km.normal('<leader>wl', '<C-w>l', 'Focus on right window')
-    km.normal('<leader>wd', '<C-w>c', 'Close current window')
+    km.normal('<leader>ws', ':split<cr>', 'window split')
+    km.normal('<leader>wv', ':vsplit<cr>', 'window vsplit')
+    km.normal('<leader>wh', '<C-w>h', 'window left')
+    km.normal('<leader>wj', '<C-w>j', 'window down')
+    km.normal('<leader>wk', '<C-w>k', 'window up')
+    km.normal('<leader>wl', '<C-w>l', 'window right')
+    km.normal('<leader>wc', '<C-w>c', 'window close')
 
     km.normal('<leader>fs', ':w<cr>', 'Save Window')
 end
