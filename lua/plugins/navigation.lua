f1f39efa9ac19be1896a52904e36752440bbf8b5
Alexander Sanchez <alex@violet.city>
2024-12-27 00:08:14 -0800

    fix neotree

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -4,53 +4,56 @@ return {
         'nvim-telescope/telescope.nvim',
         -- tag = '0.1.5',
         dependencies = {
-            'nvim-lua/plenary.nvim'
-        },
-        opts = {
-            defaults = {
-                border = false,
-            }
+            'nvim-lua/plenary.nvim',
+            -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
         config = function(_, opts)
+            local telescope = require('telescope')
+            telescope.setup({
+                defaults = {
+                    border = false,
+                }
+            })
+            -- telescope.load_extension('fzf')
             local km = require 'zk.keymaps'.keymaps
             local builtin = require 'telescope.builtin'
             local themes = require 'telescope.themes'
 
-        km.normal('<leader><leader>', function()
-            builtin.buffers(themes.get_ivy(opts))
-        end, "choose buffer")
-        km.basic('project files', function()
-            builtin.find_files(themes.get_ivy(opts))
-        end)
-        km.basic('project buffers', function()
-            builtin.buffers(themes.get_ivy(opts))
-        end)
-        km.basic('project grep', function()
-            builtin.live_grep(themes.get_ivy(opts))
-        end)
-        km.basic('telescope builtin', function ()
-            builtin.builtin(themes.get_ivy(opts))
-        end)
+            km.normal('<leader><leader>', function()
+                builtin.buffers(themes.get_ivy(opts))
+            end, "choose buffer")
+            km.basic('project files', function()
+                builtin.find_files(themes.get_ivy(opts))
+            end)
+            km.basic('project buffers', function()
+                builtin.buffers(themes.get_ivy(opts))
+            end)
+            km.basic('project grep', function()
+                builtin.live_grep(themes.get_ivy(opts))
+            end)
+            km.basic('telescope builtin', function()
+                builtin.builtin(themes.get_ivy(opts))
+            end)
 
-        km.basic('edit dev plugin config', function()
-            opts = opts or {}
-            opts.cwd = "~/.config/nvim/lua/plugins/dev"
-            builtin.find_files(themes.get_ivy(opts))
-        end)
-        km.basic('edit config', function()
-            opts = opts or {}
-            opts.cwd = "~/.config/nvim"
-            opts = themes.get_ivy(opts)
-            opts.prompt_title = 'Config Files'
-            builtin.find_files(opts)
-        end)
-        km.basic('edit plugin', function()
-            opts = opts or {}
-            opts.cwd = "~/.local/share/nvim/lazy"
-            opts = themes.get_ivy(opts)
-            opts.prompt_title = 'Plugin Files'
-            builtin.find_files(opts)
-        end)
-    end,
-}
+            km.basic('edit dev plugin config', function()
+                opts = opts or {}
+                opts.cwd = "~/.config/nvim/lua/plugins/dev"
+                builtin.find_files(themes.get_ivy(opts))
+            end)
+            km.basic('edit config', function()
+                opts = opts or {}
+                opts.cwd = "~/.config/nvim"
+                opts = themes.get_ivy(opts)
+                opts.prompt_title = 'Config Files'
+                builtin.find_files(opts)
+            end)
+            km.basic('edit plugin', function()
+                opts = opts or {}
+                opts.cwd = "~/.local/share/nvim/lazy"
+                opts = themes.get_ivy(opts)
+                opts.prompt_title = 'Plugin Files'
+                builtin.find_files(opts)
+            end)
+        end,
+    }
 }
