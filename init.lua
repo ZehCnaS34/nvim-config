aaff0ddd7eab5a057d99e21909606b2008f7ac67
ZehCnaS34 <alex@violet.city>
2025-11-10 22:36:36 -0500

    refactor init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'LazyDone',
+    callback = function ()
+        require'zk.keymaps'.setup()
+        require'zk.styling'.setup()
+    end
+})
+
 require'zk.prelude'
-require'zk.keymaps'.setup()
-require'zk.styling'.setup()
+
+
