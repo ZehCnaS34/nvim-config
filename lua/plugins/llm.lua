904bb1056f29d14be33667e39fa61a0e28051fd1
ZehCnaS34 <alex@violet.city>
2025-11-10 22:18:06 -0500

    update llm plugin

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -3,44 +3,39 @@ return {
         "yetone/avante.nvim",
         event = "VeryLazy",
         lazy = false,
-        version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+        version = false,
         opts = {
             auto_suggestions_provider = "ollama",
             debug = true,
-            provider = "ollama",
-            ollama = {
-                endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
-                model = "codellama",
+            providers = {
+                ollama = {
+                    endpoint = "http://127.0.0.1:11434",
+                    model = "codellama",
+                },
             },
+            provider = "ollama",
         },
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
         build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
         dependencies = {
             "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-            "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-            "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua",        -- for providers='copilot'
+            "echasnovski/mini.pick",
+            "nvim-telescope/telescope.nvim",
+            "hrsh7th/nvim-cmp",
+            "ibhagwan/fzf-lua",
+            "nvim-tree/nvim-web-devicons",
+            "zbirenbaum/copilot.lua",
             {
-                -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
                 event = "VeryLazy",
                 opts = {
-                    -- recommended settings
                     default = {
                         embed_image_as_base64 = false,
                         prompt_for_file_name = false,
                         drag_and_drop = {
                             insert_mode = true,
                         },
-                        -- required for Windows users
-                        use_absolute_path = true,
                     },
                 },
             },
