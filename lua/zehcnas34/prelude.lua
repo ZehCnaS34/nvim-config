33ca702d39e8bd93a8d88dcb933bf5323a5cf51c
ZehCnaS34 <alex@violet.city>
2025-11-14 10:08:09 -0500

    move more config into z34.setup

--- lua/zehcnas34/prelude.lua
+++ lua/zehcnas34/prelude.lua
@@ -19,42 +19,46 @@ Z34 = {
     modules = {}
 }
 
+function Z34.module_path(module)
+    assert(type(module) == "string", "module must be a string")
+    assert(not module:match("%."), "module must not contain a period '.'")
+    return "zehcnas34."..module
+end
+
 function Z34.reload()
     for _, module in ipairs(Z34.modules) do
-        module_name = "zehcnas34."..module
+        module_name = Z34.module_path(module)
         package.loaded[module_name]=nil
         Z34[module]=require(module_name)
     end
 end
 
 function Z34.setup(opts)
+    -- options
+    require('mini.basics').setup()
+    require('mini.extra').setup()
+    require('mini.indentscope').setup()
+    require('mini.sessions').setup()
+
+    vim.g.mapleader = ' '
+    vim.o.shiftwidth = 4
+    vim.o.tabstop = 4
+    vim.o.expandtab = true
+
+
+    -- start screen
+    local starter = require('mini.starter')
+    starter.setup({
+        items = {
+            starter.sections.builtin_actions(),
+            starter.sections.recent_files(10, false),
+            starter.sections.sessions(5, true),
+        }
+    })
+
     for _, module in ipairs(opts.modules) do
         module_name = "zehcnas34."..module
-        package.loaded[module_name]=nil
         Z34.modules[module]=require(module_name)
     end
 end
-
--- options
-require('mini.basics').setup()
-require('mini.extra').setup()
-require('mini.indentscope').setup()
-require('mini.sessions').setup()
-
-vim.g.mapleader = ' '
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.expandtab = true
-
-
--- start screen
-local starter = require('mini.starter')
-starter.setup({
-    items = {
-        starter.sections.builtin_actions(),
-        starter.sections.recent_files(10, false),
-        starter.sections.sessions(5, true),
-    }
-})
-
 
