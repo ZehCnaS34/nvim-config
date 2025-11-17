d699345bd30f033d1e7c69437f939a9220b891f8
ZehCnaS34 <alex@violet.city>
2025-11-17 11:58:10 -0500

    add all logging level functions

--- lua/zehcnas34/logger.lua
+++ lua/zehcnas34/logger.lua
@@ -9,10 +9,18 @@ API.LEVEL_DEBUG   = bit.lshift(1, 3)
 API.LEVEL_TRACE   = bit.lshift(1, 4)
 API.LEVEL         = API.LEVEL_WARNING
 
-function API.debug(msg, tabl)
-    if API.LEVEL >= API.LEVEL_DEBUG then
+
+function API.log(level, msg, table)
+    if API.LEVEL >= level then
         print(msg)
     end
 end
+
+
+API.error = function(msg, table) API.log(API.LEVEL_ERROR, msg, table) end
+API.warning = function(msg, table) API.log(API.LEVEL_WARNING, msg, table) end
+API.info = function(msg, table) API.log(API.LEVEL_INFO, msg, table) end
+API.debug = function(msg, table) API.log(API.LEVEL_DEBUG, msg, table) end
+API.trace = function(msg, table) API.log(API.LEVEL_TRACE, msg, table) end
 
 return API
