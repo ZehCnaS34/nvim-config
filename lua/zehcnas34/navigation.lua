c4c4a9ba48f5530d507d3d371fc3de27e4867ea9
ZehCnaS34 <alex@violet.city>
2025-11-30 20:09:30 -0500

    cleanup navigation

--- lua/zehcnas34/navigation.lua
+++ lua/zehcnas34/navigation.lua
@@ -10,34 +10,16 @@ local function nleadermap(suffix, rhs)
     km.set('n', '<leader>'..suffix, rhs)
 end
 
-km.set({'n'}, '<leader>pf', function()
-    -- MiniPick.builtin.files()
-    FzfLua.files()
-end)
-
-km.set({'n'}, '<leader><leader>', function()
-    MiniPick.builtin.buffers()
-end)
-
-km.set({'n'}, '<leader>pb', function()
-    MiniPick.builtin.buffers()
-end)
-
-km.set({'n'}, '<leader>pg', function()
-    MiniPick.builtin.grep_live()
-end)
-
-km.set({'n'}, '<leader>ph', function()
-    MiniPick.builtin.help()
-end)
-
-km.set({'n'}, '<leader>pe', function()
-    MiniExtra.pickers.explorer()
-end)
-
-km.set({'n'}, '-', function()
-    MiniFiles.open()
-end)
+km.set({'n'}, '<D-x>', FzfLua.commands)
+km.set({'n'}, '<D-p>', FzfLua.files)
+km.set({'n'}, '-', MiniFiles.open)
+nleadermap('pf', FzfLua.files)
+nleadermap('<leader>', MiniPick.builtin.buffers)
+nleadermap('pb', MiniPick.builtin.buffers)
+nleadermap('pg', MiniPick.builtin.grep_live)
+nleadermap('bl', FzfLua.blines)
+nleadermap('h', FzfLua.helptags)
+nleadermap('e', MiniExtra.pickers.explorer)
 
 -- windows
 nleadermap('ws', '<Cmd>split<cr>')
@@ -55,6 +37,9 @@ km.set('n', '<c-l>', '<c-w>l')
 
 -- buffer
 require'mini.bracketed'.setup()
+
+-- navigation
+nleadermap('gs', '<Cmd>term lazygit<cr>')
 
 return API
 
